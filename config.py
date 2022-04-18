f69ac5bd00da69e02ef24c92ac7cf91bef621c61
EDM 1.15 <82015596+EDM115@users.noreply.github.com>
2022-04-18 10:49:39 +0200

    Update config.py

--- config.py
+++ config.py
@@ -11,5 +11,5 @@ class Config(object):
     BOT_OWNER = int(os.environ.get("BOT_OWNER"))
     DOWNLOAD_LOCATION = f"{os.path.dirname(__file__)}/NexaBots"
     TG_MAX_SIZE = 2040108421
-    # Default chunk size (0.005 MB) Increase if you need faster downloads
+    # Default chunk size (0.005 MB → 1024*6) Increase if you need faster downloads
     CHUNK_SIZE = 1024 * 6
