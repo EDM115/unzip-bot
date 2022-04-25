9778700208ee113a8924681de7cb471378d26b26
EDM 1.15 <82015596+EDM115@users.noreply.github.com>
2022-04-25 16:35:09 +0200

    Create unzip_help.py

---
+++ unzipper/helpers/unzip_help.py
@@ -0,0 +1,80 @@
+# Copyright (c) 2022 EDM115
+
+import math
+import time
+
+from unzipper import unzipperbot as client
+from config import Config
+
+
+# Credits: SpEcHiDe's AnyDL-Bot for Progress bar + Time formatter
+async def progress_for_pyrogram(current, total, ud_type, message, start):
+    now = time.time()
+    diff = now - start
+    if round(diff % 10.00) == 0 or current == total:
+        percentage = current * 100 / total
+        speed = current / diff
+        elapsed_time = round(diff) * 1000
+        time_to_completion = round((total - current) / speed) * 1000
+        estimated_total_time = elapsed_time + time_to_completion
+        elapsed_time = TimeFormatter(milliseconds=elapsed_time)
+        estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
+        progress = "[{0}{1}] \n**Processing…** : `{2}%`\n".format(
+            ''.join(["⬢" for i in range(math.floor(percentage / 5))]),
+            ''.join(["⬡" for i in range(20 - math.floor(percentage / 5))]),
+            round(percentage, 2))
+
+        tmp = progress + "`{0} of {1}`\n**Speed :** `{2}/s`\n**ETA :** `{3}`\n".format(
+            humanbytes(current),
+            humanbytes(total),
+            humanbytes(speed),
+            estimated_total_time if estimated_total_time != '' else "0 s"
+        )
+        try:
+            await message.edit(text="{}\n {} \n\n**Powered by @EDM115bots**".format(ud_type,tmp))
+        except:
+            pass
+
+
+def humanbytes(size):
+    if not size:
+        return ""
+    power = 2**10
+    n = 0
+    Dic_powerN = {0: ' ', 1: 'Ki', 2: 'Mi', 3: 'Gi', 4: 'Ti'}
+    while size > power:
+        size /= power
+        n += 1
+    return str(round(size, 2)) + " " + Dic_powerN[n] + 'B'
+
+
+def TimeFormatter(milliseconds: int) -> str:
+    seconds, milliseconds = divmod(int(milliseconds), 1000)
+    minutes, seconds = divmod(seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    days, hours = divmod(hours, 24)
+    tmp = ((str(days) + "d, ") if days else "") + \
+        ((str(hours) + "h, ") if hours else "") + \
+        ((str(minutes) + "m, ") if minutes else "") + \
+        ((str(seconds) + "s, ") if seconds else "") + \
+        ((str(milliseconds) + "ms, ") if milliseconds else "")
+    return tmp[:-2]
+
+# Checking log channel
+def check_logs():
+    try:
+        if Config.LOGS_CHANNEL:
+            c_info = client.get_chat(chat_id=Config.LOGS_CHANNEL)
+            if c_info.type != "channel":
+                print("A chat is **not** a channel 😐")
+                return
+            elif c_info.username is not None:
+                print("A chat is **not** private 😐")
+                return
+            else:
+                client.send_message(chat_id=Config.LOGS_CHANNEL, text="`unzip-bot has successfully started !` \n\n**Powered by @EDM115bots ❤️**")
+        else:
+            print("No Log channel ID is given !")
+            exit()
+    except:
+        print("Error happened while checking Log channel 💀 Make sure you're not dumb enough to provide a wrong Log channel ID 🧐")
