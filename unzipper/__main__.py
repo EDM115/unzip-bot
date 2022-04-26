efe58a143bc2243c90c178f1b020e45710e2d35d
EDM 1.15 <dev.edm115@gmail.com>
2022-04-26 19:38:51 +0200

    Update __main__.py

--- unzipper/__main__.py
+++ unzipper/__main__.py
@@ -4,14 +4,11 @@ import os
 
 from pyrogram import idle
 from . import unzipperbot
-from .helpers.unzip_help import check_logs
 from config import Config
 
 if __name__ == "__main__" :
     if not os.path.isdir(Config.DOWNLOAD_LOCATION):
         os.makedirs(Config.DOWNLOAD_LOCATION)
     unzipperbot.start()
-    print("Checking Log channel…")
-    check_logs()
     print("Bot is active now ! Join @EDM115bots")
     idle()
