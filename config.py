cc43ff8bfce0cb54ad4459bfcab6341367563e62
EDM 1.15 <dev.edm115@gmail.com>
2022-04-26 19:36:44 +0200

    Update config.py

--- config.py
+++ config.py
@@ -6,9 +6,6 @@ class Config(object):
     APP_ID = int(os.environ.get("APP_ID"))
     API_HASH = os.environ.get("API_HASH")
     BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    LOGS_CHANNEL = int(os.environ.get("LOGS_CHANNEL"))
-    MONGODB_URL = os.environ.get("MONGODB_URL")
-    BOT_OWNER = int(os.environ.get("BOT_OWNER"))
     DOWNLOAD_LOCATION = f"{os.path.dirname(__file__)}/Downloaded"
     TG_MAX_SIZE = 2040108421
     # Default chunk size (0.005 MB → 1024*6) Increase if you need faster downloads
