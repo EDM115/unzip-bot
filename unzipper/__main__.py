c22ef0b7086cdab5b49b32ed3d0a70a038e25263
EDM 1.15 <dev.edm115@gmail.com>
2022-05-07 15:23:34 +0200

    Update __main__.py

--- unzipper/__main__.py
+++ unzipper/__main__.py
@@ -8,6 +8,13 @@ from . import unzipperbot
 from .helpers.unzip_help import check_logs
 from config import Config
 
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+LOGGER = logging.getLogger(__name__)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
 if __name__ == "__main__" :
     if not os.path.isdir(Config.DOWNLOAD_LOCATION):
         os.makedirs(Config.DOWNLOAD_LOCATION)
