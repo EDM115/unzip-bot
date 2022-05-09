6b69084cd7337453effb7e9015d2c77da83f8d81
EDM 1.15 <dev.edm115@gmail.com>
2022-05-09 08:27:53 +0200

    Better database
    Upload mode is now in a specific ``mode_db` and no longer into the banned one
    Real changes here

--- unzipper/helpers/database.py
+++ unzipper/helpers/database.py
@@ -32,8 +32,7 @@ async def is_user_in_db(user_id):
     is_exist = await user_db.find_one({"user_id": u_id})
     if is_exist:
         return True
-    else:
-        return False
+    return False
 
 async def count_users():
     users = await user_db.count_documents({})
@@ -42,8 +41,7 @@ async def count_users():
 async def get_users_list():
     return [users_list async for users_list in user_db.find({})]
 
-
-# Banned users database (I don't know why tf i added this, but who cares)
+# Banned users database
 b_user_db = unzipper_db["banned_users_db"]
 
 async def add_banned_user(user_id):
@@ -67,8 +65,7 @@ async def is_user_in_bdb(user_id):
     is_exist = await b_user_db.find_one({"banned_user_id": u_id})
     if is_exist:
         return True
-    else:
-        return False
+    return False
 
 async def count_banned_users():
     users = await b_user_db.count_documents({})
@@ -77,15 +74,14 @@ async def count_banned_users():
 async def get_banned_users_list():
     return [banned_users_list async for banned_users_list in b_user_db.find({})]
 
-# Gonna cry ?
 async def check_user(message):
     # Checking if user is banned
     is_banned = await is_user_in_bdb(message.from_user.id)
     if is_banned:
-        await message.reply("**Sorry you're banned 💀** \n\nReport this at @EDM115 if you think this is a mistake")
+        await message.reply("**Sorry you're banned 💀** \n\nReport this at @EDM115 if you think this is a mistake, I can unban you
         await message.stop_propagation()
         return
-    # Chacking if user already in db
+    # Cheking if user already in db
     is_in_db = await is_user_in_db(message.from_user.id)
     if not is_in_db:
         await add_user(message.from_user.id)
@@ -103,19 +99,18 @@ async def check_user(message):
             )
     await message.continue_propagation()
 
-
 # Upload mode
 mode_db = unzipper_db["upload_mode_db"]
 
 async def set_upload_mode(user_id, mode):
-    is_exist = await b_user_db.find_one({"_id": user_id})
+    is_exist = await mode_db.find_one({"_id": user_id})
     if is_exist:
-        await b_user_db.update_one({"_id": user_id}, {"$set": {"mode": mode}})
+        await mode_db.update_one({"_id": user_id}, {"$set": {"mode": mode}})
     else:
-        await b_user_db.insert_one({"_id": user_id, "mode": mode})
+        await mode_db.insert_one({"_id": user_id, "mode": mode})
 
 async def get_upload_mode(user_id):
-    umode = await b_user_db.find_one({"_id": user_id})
+    umode = await mode_db.find_one({"_id": user_id})
     if umode:
         return umode["mode"]
     else:
