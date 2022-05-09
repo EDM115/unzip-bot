3f3827d96c41d19700aa6ba04257b38198a1c2b0
EDM 1.15 <dev.edm115@gmail.com>
2022-05-09 08:41:49 +0200

    Update unzip_help.py

--- unzipper/helpers/unzip_help.py
+++ unzipper/helpers/unzip_help.py
@@ -2,10 +2,10 @@
 
 import math
 import time
+from typing import List, Union
 
 from unzipper import unzipperbot as client
 from config import Config
-
 
 # Credits: SpEcHiDe's AnyDL-Bot for Progress bar + Time formatter
 async def progress_for_pyrogram(current, total, ud_type, message, start):
@@ -90,46 +90,42 @@ def humanbytes(size):
     return str(round(size, 2)) + " " + Dic_powerN[n] + 'B'
 
 # Workaround
-#from typing import List, Union
-#class HumanBytes:
-#    METRIC_LABELS: List[str] = ["B", "kB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"]
-#    BINARY_LABELS: List[str] = ["B", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB", "YiB"]
-#    PRECISION_OFFSETS: List[float] = [0.5, 0.05, 0.005, 0.0005] # PREDEFINED FOR SPEED.
-#    PRECISION_FORMATS: List[str] = ["{}{:.0f} {}", "{}{:.1f} {}", "{}{:.2f} {}", "{}{:.3f} {}"] # PREDEFINED FOR SPEED.
-#    @staticmethod
-#    def format(num: Union[int, float], metric: bool=False, precision: int=1) -> str:
-#        """
-#        Human-readable formatting of bytes, using binary (powers of 1024)
-#        or metric (powers of 1000) representation.
-#        """
-#        assert isinstance(num, (int, float)), "num must be an int or float"
-#        assert isinstance(metric, bool), "metric must be a bool"
-#        assert isinstance(precision, int) and precision >= 0 and precision <= 3, "precision must be an int (range 0-3)"
-#        unit_labels = HumanBytes.METRIC_LABELS if metric else HumanBytes.BINARY_LABELS
-#        last_label = unit_labels[-1]
-#        unit_step = 1000 if metric else 1024
-#        unit_step_thresh = unit_step - HumanBytes.PRECISION_OFFSETS[precision]
-#        is_negative = num < 0
-#        if is_negative: # Faster than ternary assignment or always running abs().
-#            num = abs(num)
-#        for unit in unit_labels:
-#            if num < unit_step_thresh:
-#                # VERY IMPORTANT:
-#                # Only accepts the CURRENT unit if we're BELOW the threshold where
-#                # float rounding behavior would place us into the NEXT unit: F.ex.
-#                # when rounding a float to 1 decimal, any number ">= 1023.95" will
-#                # be rounded to "1024.0". Obviously we don't want ugly output such
-#                # as "1024.0 KiB", since the proper term for that is "1.0 MiB".
-#                break
-#            if unit != last_label:
-#                # We only shrink the number if we HAVEN'T reached the last unit.
-#                # NOTE: These looped divisions accumulate floating point rounding
-#                # errors, but each new division pushes the rounding errors further
-#                # and further down in the decimals, so it doesn't matter at all.
-#                num /= unit_step
-#        return HumanBytes.PRECISION_FORMATS[precision].format("-" if is_negative else "", num, unit)
+class HumanBytes:
+    METRIC_LABELS: List[str] = ["B", "kB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"]
+    BINARY_LABELS: List[str] = ["B", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB", "YiB"]
+    PRECISION_OFFSETS: List[float] = [0.5, 0.05, 0.005, 0.0005] # PREDEFINED FOR SPEED.
+    PRECISION_FORMATS: List[str] = ["{}{:.0f} {}", "{}{:.1f} {}", "{}{:.2f} {}", "{}{:.3f} {}"] # PREDEFINED FOR SPEED.
+    @staticmethod
+    def format(num: Union[int, float], metric: bool=False, precision: int=1) -> str:
+        # Human-readable formatting of bytes, using binary (powers of 1024)
+        # or metric (powers of 1000) representation.
+        assert isinstance(num, (int, float)), "num must be an int or float"
+        assert isinstance(metric, bool), "metric must be a bool"
+        assert isinstance(precision, int) and precision >= 0 and precision <= 3, "precision must be an int (range 0-3)"
+        unit_labels = HumanBytes.METRIC_LABELS if metric else HumanBytes.BINARY_LABELS
+        last_label = unit_labels[-1]
+        unit_step = 1000 if metric else 1024
+        unit_step_thresh = unit_step - HumanBytes.PRECISION_OFFSETS[precision]
+        is_negative = num < 0
+        if is_negative: # Faster than ternary assignment or always running abs().
+            num = abs(num)
+        for unit in unit_labels:
+            if num < unit_step_thresh:
+                # VERY IMPORTANT:
+                # Only accepts the CURRENT unit if we're BELOW the threshold where
+                # float rounding behavior would place us into the NEXT unit: F.ex.
+                # when rounding a float to 1 decimal, any number ">= 1023.95" will
+                # be rounded to "1024.0". Obviously we don't want ugly output such
+                # as "1024.0 KiB", since the proper term for that is "1.0 MiB".
+                break
+            if unit != last_label:
+                # We only shrink the number if we HAVEN'T reached the last unit.
+                # NOTE: These looped divisions accumulate floating point rounding
+                # errors, but each new division pushes the rounding errors further
+                # and further down in the decimals, so it doesn't matter at all.
+                num /= unit_step
+        return HumanBytes.PRECISION_FORMATS[precision].format("-" if is_negative else "", num, unit)
 # Usage : HumanBytes.format({value}))
-
 
 def TimeFormatter(milliseconds: int) -> str:
     seconds, milliseconds = divmod(int(milliseconds), 1000)
@@ -141,6 +137,16 @@ def TimeFormatter(milliseconds: int) -> str:
         ((str(minutes) + "m, ") if minutes else "") + \
         ((str(seconds) + "s, ") if seconds else "") + \
         ((str(milliseconds) + "ms, ") if milliseconds else "")
+    return tmp[:-2]
+
+def timeformat_sec(seconds: int) -> str:
+    minutes, seconds = divmod(seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    days, hours = divmod(hours, 24)
+    tmp = ((str(days) + "d, ") if days else "") + \
+        ((str(hours) + "h, ") if hours else "") + \
+        ((str(minutes) + "m, ") if minutes else "") + \
+        ((str(seconds) + "s, ") if seconds else "")
     return tmp[:-2]
 
 # Checking log channel
@@ -155,7 +161,7 @@ def check_logs():
                 print("A chat is **not** private 😐")
                 return
             else:
-                client.send_message(chat_id=Config.LOGS_CHANNEL, text="`unzip-bot has successfully started !` \n\n**Powered by @EDM115bots ❤️**")
+                client.send_message(chat_id=Config.LOGS_CHANNEL, text=f"`unzip-bot has successfully started !` \n\n**Powered by @EDM115bots ❤️**")
         else:
             print("No Log channel ID is given !")
             exit()
