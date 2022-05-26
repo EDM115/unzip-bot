7995961e93acd8e19b3231af0d7b88ecdd894870
EDM 1.15 <dev.edm115@gmail.com>
2022-05-26 20:02:27 +0200

    WARN

--- unzipper/__init__.py
+++ unzipper/__init__.py
@@ -18,7 +18,7 @@ unzipperbot = Client(
     )
 
 logging.basicConfig(
-    level=logging.INFO,
+    level=logging.WARN,
     # Can't be done due to https://github.com/EDM115/unzip-bot/blob/3f951a29cd2a19d592bbc3e65bbbbb678fa5cee3/unzipper/modules/commands.py#L242
     # Reference for future implementation : https://stackoverflow.com/questions/5013532/open-file-by-filename-wildcard
     #actualtime=time.strftime(%d-%m-%Y)
@@ -28,6 +28,6 @@ logging.basicConfig(
     format="%(asctime)s - %(levelname)s - %(name)s - %(threadName)s - %(message)s"
 )
 LOGGER = logging.getLogger(__name__)
-logging.getLogger("pyrogram").setLevel(logging.INFO)
+logging.getLogger("pyrogram").setLevel(logging.WARN)
 logging.getLogger("motor").setLevel(logging.INFO)
 logging.getLogger("aiohttp").setLevel(logging.INFO)
