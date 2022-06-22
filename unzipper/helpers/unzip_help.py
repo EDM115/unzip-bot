af2d464502ae7cfb98234ca430284a431ff8cd23
EDM 1.15 <dev@edm115.eu.org>
2022-06-22 16:32:32 +0200

    check_logs() that works

--- unzipper/helpers/unzip_help.py
+++ unzipper/helpers/unzip_help.py
@@ -4,7 +4,7 @@ import math
 import time
 from typing import List, Union
 
-from unzipper import unzipperbot as client
+from unzipper import unzipperbot as client, LOGGER
 from config import Config
 
 # Credits: SpEcHiDe's AnyDL-Bot for Progress bar + Time formatter
@@ -38,50 +38,6 @@ async def progress_for_pyrogram(current, total, ud_type, message, start):
         except:
             pass
 
-# Workaround :
-"""
-def exponential_moving_average(data, samples=0, smoothing=0.02):
-    '''
-    data: an array of all values.
-    samples: how many previous data samples are avraged. Set to 0 to average all data points.
-    smoothing: a value between 0-1, 1 being a linear average (no falloff).
-    '''
-
-    if len(data) == 1:
-        return data[0]
-
-    if samples == 0 or samples > len(data):
-        samples = len(data)
-
-    average = sum(data[-samples:]) / samples
-    last_speed = data[-1]
-    return (smoothing * last_speed) + ((1 - smoothing) * average)
-
-input_data = [4.5, 8.21, 8.7, 5.8, 3.8, 2.7, 2.5, 7.1, 9.3, 2.1, 3.1, 9.7, 5.1, 6.1, 9.1, 5.0, 1.6, 6.7, 5.5, 3.2] # this would be a constant stream of download speeds as you go, pre-defined here for illustration
-
-data = []
-ema_data = []
-
-for sample in input_data:
-    data.append(sample)
-    average_value = exponential_moving_average(data)
-    ema_data.append(average_value)
-
-# print it out for visualization
-for i in range(len(data)):
-    print("REAL: ", data[i])
-    print("EMA:  ", ema_data[i])
-    print("--")
-"""
-
-"""
-averageSpeed = SMOOTHING_FACTOR * lastSpeed + (1-SMOOTHING_FACTOR) * averageSpeed;
-
-SMOOTHING_FACTOR is a number between 0 and 1. The higher this number, the faster older samples are discarded. As you can see in the formula, when SMOOTHING_FACTOR is 1 you are simply using the value of your last observation. When SMOOTHING_FACTOR is 0 averageSpeed never changes. So, you want something in between, and usually a low value to get decent smoothing. I've found that 0.005 provides a pretty good smoothing value for an average download speed.
-lastSpeed is the last measured download speed. You can get this value by running a timer every second or so to calculate how many bytes have downloaded since the last time you ran it.
-averageSpeed is, obviously, the number that you want to use to calculate your estimated time remaining. Initialize this to the first lastSpeed measurement you get.
-"""
-
 def humanbytes(size):
     if not size:
         return ""
@@ -93,7 +49,6 @@ def humanbytes(size):
         n += 1
     return str(round(size, 2)) + " " + Dic_powerN[n] + 'B'
 
-# Workaround
 class HumanBytes:
     METRIC_LABELS: List[str] = ["B", "kB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"]
     BINARY_LABELS: List[str] = ["B", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB", "YiB"]
@@ -158,16 +113,16 @@ def check_logs():
     try:
         if Config.LOGS_CHANNEL:
             c_info = client.get_chat(chat_id=Config.LOGS_CHANNEL)
-            if c_info.type != "channel":
-                print("A chat is **not** a channel 😐")
-                return
+            if c_info.type != "channel" and c_info.type != "group" and c_info.type != "supergroup":
+                LOGGER.warn("A chat is **not** a channel 😐")
+                return False
             elif c_info.username is not None:
-                print("A chat is **not** private 😐")
-                return
+                LOGGER.warn("A chat is **not** private 😐")
+                return False
             else:
-                client.send_message(chat_id=Config.LOGS_CHANNEL, text=f"`unzip-bot has successfully started !` \n\n**Powered by @EDM115bots ❤️**")
+                return True
         else:
-            print("No Log channel ID is given !")
+            LOGGER.warn("No Log channel ID is given !")
             exit()
     except:
         print("Error happened while checking Log channel 💀 Make sure you're not dumb enough to provide a wrong Log channel ID 🧐")
