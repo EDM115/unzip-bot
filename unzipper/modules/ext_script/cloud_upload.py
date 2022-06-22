9828a513a36ac5f119c5327d5278ad475a55a790
EDM 1.15 <dev@edm115.eu.org>
2022-06-22 22:52:20 +0200

    Better like this haha :)

--- unzipper/modules/ext_script/cloud_upload.py
+++ unzipper/modules/ext_script/cloud_upload.py
@@ -1,13 +1,13 @@
 import subprocess
 
-async def terminal(command):
+async def jsonized(command):
     run = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     shell_ouput = run.stdout.read()[:-1].decode("utf-8")
-    return shell_ouput
+    return shell_ouput.replace("true", "True")
 
 async def bayfiles(file, url):
     try:
-        uploaded = await terminal(f"curl -F 'file=@{file}' {url}").replace("true", "True")
+        uploaded = await jsonized(f"curl -F 'file=@{file}' {url}")
     except:
         uploaded = "Error happened on BayFiles upload (check connection, or retry later)"
     return uploaded
