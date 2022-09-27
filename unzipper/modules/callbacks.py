a9e7afa22ee8aa260d3e2e7eb08c1253b3017aac
EDM 1.15 <dev@edm115.eu.org>
2022-09-27 12:34:22 +0200

    Update callbacks.py

--- unzipper/modules/callbacks.py
+++ unzipper/modules/callbacks.py
@@ -8,6 +8,7 @@ from urllib.parse import unquote
 
 from aiofiles import open as openfile
 from aiohttp import ClientSession
+from pyromod import listen
 from pyrogram import Client
 from pyrogram.errors import ReplyMarkupTooLong
 from pyrogram.types import CallbackQuery
