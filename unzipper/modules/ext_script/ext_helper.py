6e03c547fe04fedabf82cfe4e9ca2399ec226e94
EDM 1.15 <dev@edm115.eu.org>
2022-09-27 20:19:51 +0200

    Update ext_helper.py

--- unzipper/modules/ext_script/ext_helper.py
+++ unzipper/modules/ext_script/ext_helper.py
@@ -57,7 +57,7 @@ async def extr_files(protected, path, archive_path, password=None):
 # Split files
 async def split_files(iinput, ooutput):
     # Workaround : https://ccm.net/computing/linux/4327-split-a-file-into-several-parts-in-linux/
-    command = f"split -a 3 -e --numeric-suffixes=001 2GB -d {iinput} {ooutput}"
+    command = f"split -a 3 -e --numeric-suffixes=001 -b 2GB {iinput} {ooutput}"
     await run_cmds_on_cr(__run_cmds_unzipper, cmd=command)
     spdir = ooutput.replace("/" + ooutput.split("/")[-1], "")
     splittedfiles = await get_files(spdir)
