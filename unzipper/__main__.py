9f8258d9a2a076a0aa3cd153784b1081337d4d0b
Restyled.io <commits@restyled.io>
2022-09-29 14:07:01 +0000

    Restyled by black

--- unzipper/__main__.py
+++ unzipper/__main__.py
@@ -42,8 +42,7 @@ while running:
         unzipperbot.start()
         starttime = time.strftime("%Y/%m/%d - %H:%M:%S")
         unzipperbot.send_message(
-            chat_id=Config.LOGS_CHANNEL, text=Messages.START_TXT.format(
-                starttime)
+            chat_id=Config.LOGS_CHANNEL, text=Messages.START_TXT.format(starttime)
         )
         dl_thumbs()
         LOGGER.info("Checking Log channel…")
