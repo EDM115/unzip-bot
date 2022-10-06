2b4f40f86759c715a24114e8667016b2e91efe38
EDM 1.15 <dev@edm115.eu.org>
2022-10-06 13:15:48 +0200

    Update unzip_help.py

--- unzipper/helpers/unzip_help.py
+++ unzipper/helpers/unzip_help.py
@@ -2,44 +2,48 @@
 import math
 import time
 from typing import List, Union
+from pyrogram import Client
 
 
 # Credits: SpEcHiDe's AnyDL-Bot for Progress bar + Time formatter
-async def progress_for_pyrogram(current, total, ud_type, message, start):
-    now = time.time()
-    diff = now - start
-    if round(diff % 10.00) == 0 or current == total:
-        timenow = round(time.time() - start) * 1000
-        percentage = current * 100 / total
-        speed = current / diff
-        elapsed_time = round(diff) * 1000
-        time_to_completion = round((total - current) / speed) * 1000
-        estimated_total_time = elapsed_time + time_to_completion
-        elapsed_time = TimeFormatter(milliseconds=elapsed_time)
-        """
-        if ((int(estimated_total_time) > int(elapsed_time)) and (int(timenow) > int(elapsed_time))) or ((int(estimated_total_time) > int(elapsed_time)) and (int(percentage) > 50)):
-           estimated_total_time -= elapsed_time / 10
-        """
-        estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
-        progress = "[{0}{1}] \n**Processing…** : `{2}%`\n".format(
-            "".join(["⬢" for i in range(math.floor(percentage / 5))]),
-            "".join(["⬡" for i in range(20 - math.floor(percentage / 5))]),
-            round(percentage, 2),
-        )
+async def progress_for_pyrogram(current, total, ud_type, message, start, client, status):
+    if !status :
+        now = time.time()
+        diff = now - start
+        if round(diff % 10.00) == 0 or current == total:
+            timenow = round(time.time() - start) * 1000
+            percentage = current * 100 / total
+            speed = current / diff
+            elapsed_time = round(diff) * 1000
+            time_to_completion = round((total - current) / speed) * 1000
+            estimated_total_time = elapsed_time + time_to_completion
+            elapsed_time = TimeFormatter(milliseconds=elapsed_time)
+            """
+            if ((int(estimated_total_time) > int(elapsed_time)) and (int(timenow) > int(elapsed_time))) or ((int(estimated_total_time) > int(elapsed_time)) and (int(percentage) > 50)):
+                estimated_total_time -= elapsed_time / 10
+            """
+            estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
+            progress = "[{0}{1}] \n**Processing…** : `{2}%`\n".format(
+                "".join(["⬢" for i in range(math.floor(percentage / 5))]),
+                "".join(["⬡" for i in range(20 - math.floor(percentage / 5))]),
+                round(percentage, 2),
+            )
 
-        tmp = progress + "`{0} of {1}`\n**Speed :** `{2}/s`\n**ETA :** `{3}`\n".format(
-            humanbytes(current),
-            humanbytes(total),
-            humanbytes(speed),
-            estimated_total_time
-            if estimated_total_time != "" or percentage != "100" else "0 s",
-        )
-        try:
-            await message.edit(
-                text="{}\n {} \n\n**Powered by @EDM115bots**".format(
-                    ud_type, tmp))
-        except:
-            pass
+            tmp = progress + "`{0} of {1}`\n**Speed :** `{2}/s`\n**ETA :** `{3}`\n".format(
+                humanbytes(current),
+                humanbytes(total),
+                humanbytes(speed),
+                estimated_total_time
+                if estimated_total_time != "" or percentage != "100" else "0 s",
+            )
+            try:
+                await message.edit(
+                    text="{}\n {} \n\n**Powered by @EDM115bots**".format(
+                        ud_type, tmp))
+            except:
+                pass
+        else:
+            return await client.stop_transmission()
 
 
 def humanbytes(size):
