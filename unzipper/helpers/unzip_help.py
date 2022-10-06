d5259f68c76096586f0f7a645a0e782bce9b3d11
Restyled.io <commits@restyled.io>
2022-10-06 11:36:59 +0000

    Restyled by yapf

--- unzipper/helpers/unzip_help.py
+++ unzipper/helpers/unzip_help.py
@@ -7,9 +7,8 @@ from pyrogram import Client
 
 
 # Credits: SpEcHiDe's AnyDL-Bot for Progress bar + Time formatter
-async def progress_for_pyrogram(
-    current, total, ud_type, message, start, client, status
-):
+async def progress_for_pyrogram(current, total, ud_type, message, start,
+                                client, status):
     if not status:
         now = time.time()
         diff = now - start
@@ -25,7 +24,8 @@ async def progress_for_pyrogram(
             if ((int(estimated_total_time) > int(elapsed_time)) and (int(timenow) > int(elapsed_time))) or ((int(estimated_total_time) > int(elapsed_time)) and (int(percentage) > 50)):
                 estimated_total_time -= elapsed_time / 10
             """
-            estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
+            estimated_total_time = TimeFormatter(
+                milliseconds=estimated_total_time)
             progress = "[{0}{1}] \n**Processing…** : `{2}%`\n".format(
                 "".join(["⬢" for i in range(math.floor(percentage / 5))]),
                 "".join(["⬡" for i in range(20 - math.floor(percentage / 5))]),
@@ -33,20 +33,18 @@ async def progress_for_pyrogram(
             )
 
             tmp = (
-                progress
-                + "`{0} of {1}`\n**Speed :** `{2}/s`\n**ETA :** `{3}`\n".format(
+                progress +
+                "`{0} of {1}`\n**Speed :** `{2}/s`\n**ETA :** `{3}`\n".format(
                     humanbytes(current),
                     humanbytes(total),
                     humanbytes(speed),
-                    estimated_total_time
-                    if estimated_total_time != "" or percentage != "100"
-                    else "0 s",
-                )
-            )
+                    estimated_total_time if estimated_total_time != ""
+                    or percentage != "100" else "0 s",
+                ))
             try:
                 await message.edit(
-                    text="{}\n {} \n\n**Powered by @EDM115bots**".format(ud_type, tmp)
-                )
+                    text="{}\n {} \n\n**Powered by @EDM115bots**".format(
+                        ud_type, tmp))
             except:
                 pass
         else:
@@ -70,13 +68,11 @@ def TimeFormatter(milliseconds: int) -> str:
     minutes, seconds = divmod(seconds, 60)
     hours, minutes = divmod(minutes, 60)
     days, hours = divmod(hours, 24)
-    tmp = (
-        ((str(days) + "d, ") if days else "")
-        + ((str(hours) + "h, ") if hours else "")
-        + ((str(minutes) + "m, ") if minutes else "")
-        + ((str(seconds) + "s, ") if seconds else "")
-        + ((str(milliseconds) + "ms, ") if milliseconds else "")
-    )
+    tmp = (((str(days) + "d, ") if days else "") +
+           ((str(hours) + "h, ") if hours else "") +
+           ((str(minutes) + "m, ") if minutes else "") +
+           ((str(seconds) + "s, ") if seconds else "") +
+           ((str(milliseconds) + "ms, ") if milliseconds else ""))
     return tmp[:-2]
 
 
@@ -84,12 +80,10 @@ def timeformat_sec(seconds: int) -> str:
     minutes, seconds = divmod(int(seconds), 60)
     hours, minutes = divmod(minutes, 60)
     days, hours = divmod(hours, 24)
-    tmp = (
-        ((str(days) + "d, ") if days else "")
-        + ((str(hours) + "h, ") if hours else "")
-        + ((str(minutes) + "m, ") if minutes else "")
-        + ((str(seconds) + "s, ") if seconds else "")
-    )
+    tmp = (((str(days) + "d, ") if days else "") +
+           ((str(hours) + "h, ") if hours else "") +
+           ((str(minutes) + "m, ") if minutes else "") +
+           ((str(seconds) + "s, ") if seconds else ""))
     return tmp[:-2]
 
 
