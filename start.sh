fed28aa10a713702b4073cde69534aa381f1f32a
EDM115 <dev@edm115.eu.org>
2023-05-19 10:25:24 +0200

    v6 on its way

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 echo "
 🔥 Unzip Bot 🔥
 
-Copyright (c) 2023 EDM115
+Copyright (c) 2022 - 2023 EDM115
 
---> Join @EDM115bots
+--> Join @EDM115bots on Telegram
 --> Follow EDM115 on Github
 "
 python3 -m unzipper
