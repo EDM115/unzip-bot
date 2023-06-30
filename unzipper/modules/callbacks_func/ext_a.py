7317b06ccef98ac5434edfe4c4358064a79a53e6
EDM115 <dev@edm115.eu.org>
2023-06-30 13:49:21 +0200

    refactor ?

--- unzipper/modules/callbacks_func/ext_a.py
+++ unzipper/modules/callbacks_func/ext_a.py
@@ -1,2 +1,49 @@
-def ext_a(query):
-	pass
+# Copyright (c) 2023 EDM115
+
+import shutil
+
+from config import Config
+from unzipper import LOGGER
+from unzipper.helpers.database import del_ongoing_task, update_uploaded
+from unzipper.modules.bot_data import Messages
+from unzipper.modules.ext_script.ext_helper import get_files
+from unzipper.modules.ext_script.up_helper import send_file
+
+async def ext_a(query, unzip_bot, log_msg, sent_files, archive_msg):
+	user_id = query.from_user.id
+	spl_data = query.data.split("|")
+	LOGGER.warning("ext_a spl_data : " + str(spl_data))
+	file_path = f"{Config.DOWNLOAD_LOCATION}/{spl_data[1]}/extracted"
+	paths = await get_files(path=file_path)
+	LOGGER.warning("ext_a paths : " + str(paths))
+	if not paths:
+		try:
+			shutil.rmtree(f"{Config.DOWNLOAD_LOCATION}/{spl_data[1]}")
+		except:
+			pass
+		await del_ongoing_task(user_id)
+		return await query.message.edit("I've already sent you those files 🙂")
+
+	await query.answer("Trying to send all files to you… Please wait")
+	for file in paths:
+		LOGGER.info("ext_a file in paths : " + file)
+		sent_files += 1
+		await send_file(
+			unzip_bot=unzip_bot,
+			c_id=spl_data[2],
+			doc_f=file,
+			query=query,
+			full_path=f"{Config.DOWNLOAD_LOCATION}/{spl_data[1]}",
+			log_msg=log_msg,
+			split=False,
+		)
+
+	await query.message.edit("**Successfully uploaded ✅**\n\n**Join @EDM115bots ❤️**")
+	await log_msg.reply(Messages.HOW_MANY_UPLOADED.format(sent_files))
+	await update_uploaded(user_id, upload_count=sent_files)
+	await del_ongoing_task(user_id)
+	try:
+		shutil.rmtree(f"{Config.DOWNLOAD_LOCATION}/{spl_data[1]}")
+	except Exception as e:
+		await query.message.edit(Messages.ERROR_TXT.format(e))
+		await archive_msg.reply(Messages.ERROR_TXT.format(e))
--- unzipper/modules/callbacks_func/file.py
+++ unzipper/modules/callbacks_func/file.py
@@ -1,2 +1,51 @@
-def file(query):
-	pass
+# Copyright (c) 2023 EDM115
+
+import os
+import re
+
+from fnmatch import fnmatch
+from time import time
+
+from config import Config
+from unzipper.helpers.database import del_ongoing_task
+from unzipper.helpers.unzip_help import progress_for_pyrogram, humanbytes, extentions_list
+from unzipper.modules.bot_data import Messages
+from unzipper.modules.callbacks import split_file_pattern
+
+async def file(query, r_message, user_id, log_msg, splitted_data, download_path):
+	if r_message.document is None:
+		await del_ongoing_task(user_id)
+		return await query.message.edit("Give me an archive to extract 😐")
+	fname = r_message.document.file_name
+	rfnamebro = fname
+	archive_msg = await r_message.forward(chat_id=Config.LOGS_CHANNEL)
+	await log_msg.edit(
+		Messages.LOG_TXT.format(
+			user_id, fname,
+			humanbytes(r_message.document.file_size)
+		)
+	)
+	# Checks if it's actually an archive
+	# fext = (pathlib.Path(fname).suffix).casefold()
+	if splitted_data[2] != "thumb":
+		fext = fname.split(".")[-1].casefold()
+		if (fnmatch(fext, extentions_list["split"][0]) or fext in extentions_list["split"] or bool(re.search(split_file_pattern, fname))):
+			await del_ongoing_task(user_id)
+			return await query.message.edit("Splitted archives can't be processed yet")
+		if fext not in extentions_list["archive"]:
+			await del_ongoing_task(user_id)
+			return await query.message.edit("This file is NOT an archive 😐\nIf you believe it's an error, send the file to **@EDM115**")
+	# Makes download dir
+	os.makedirs(download_path)
+	s_time = time()
+	location = f"{download_path}/archive_from_{user_id}{os.path.splitext(fname)[1]}"
+	archive = await r_message.download(
+		file_name=location,
+		progress=progress_for_pyrogram,
+		progress_args=(
+			"**Trying to download… Please wait** \n",
+			query.message,
+			s_time,
+		),
+	)
+	e_time = time()
