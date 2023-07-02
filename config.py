83c54bad25161b6a5224ada4c0ffc0326da26c8c
EDM115 <dev@edm115.eu.org>
2023-07-02 11:05:01 +0200

    exact 2000 MiB (2097152000b)

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ class Config:
     BOT_OWNER = int(os.environ.get("BOT_OWNER"))
     DOWNLOAD_LOCATION = f"{os.path.dirname(__file__)}/Downloaded"
     THUMB_LOCATION = f"{os.path.dirname(__file__)}/Thumbnails"
-    TG_MAX_SIZE = 2000000000 #2040108421
+    TG_MAX_SIZE = 2097152000
     # Default chunk size (0.005 MB → 1024*6) Increase if you need faster downloads
     CHUNK_SIZE = 1024 * 6
     BOT_THUMB = f"{os.path.dirname(__file__)}/bot_thumb.jpg"
