c68135bf6c2022e3ce7860d03b5f3428152d3999
EDM115 <dev@edm115.eu.org>
2023-07-09 10:13:29 +0200

    50 tasks

--- config.py
+++ config.py
@@ -15,6 +15,6 @@ class Config:
     # Default chunk size (0.005 MB → 1024*6) Increase if you need faster downloads
     CHUNK_SIZE = 1024 * 1024 * 5  # 5 MB
     BOT_THUMB = f"{os.path.dirname(__file__)}/bot_thumb.jpg"
-    MAX_CONCURRENT_TASKS = 25
+    MAX_CONCURRENT_TASKS = 50
     MAX_TASK_DURATION_EXTRACT = 45 * 60  # 45 minutes (in seconds)
     MAX_TASK_DURATION_MERGE = 90 * 60  # 1 hour 30 minutes (in seconds)
