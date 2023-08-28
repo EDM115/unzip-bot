45cea3eae602935671b9ab2aa5da21357ed0f988
EDM115 <dev@edm115.eu.org>
2023-08-28 17:59:33 +0200

    minor changes

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ class Config:
     THUMB_LOCATION = f"{os.path.dirname(__file__)}/Thumbnails"
     TG_MAX_SIZE = 2097152000
     # Default chunk size (0.005 MB → 1024*6) Increase if you need faster downloads
-    CHUNK_SIZE = 1024 * 1024 * 5  # 5 MB
+    CHUNK_SIZE = 1024 * 1024 * 10  # 10 MB
     BOT_THUMB = f"{os.path.dirname(__file__)}/bot_thumb.jpg"
     MAX_CONCURRENT_TASKS = 50
     MAX_TASK_DURATION_EXTRACT = 45 * 60  # 45 minutes (in seconds)
