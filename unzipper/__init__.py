741c019b1b7cfd10ce9051d395140dc8b614bf76
EDM115 <dev@edm115.eu.org>
2023-08-31 15:13:57 +0200

    yes

--- unzipper/__init__.py
+++ unzipper/__init__.py
@@ -9,13 +9,13 @@ from config import Config
 
 boottime = time.time()
 
-plugins = dict(root="unzipper/modules")
+plugins = dict(root="modules")
 premiumuser = Client(
     "PremiumUser",
     session_string=Config.SESSION_STRING,
     api_id=Config.APP_ID,
     api_hash=Config.API_HASH,
-    in_memory=False,
+    plugins=plugins,
     sleep_threshold=10,
     max_concurrent_transmissions=3,
 )
