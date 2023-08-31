96b7da046fd99ed8d7dba29646dc299ee31bbaeb
EDM115 <dev@edm115.eu.org>
2023-08-31 15:00:18 +0200

    hope

--- unzipper/__init__.py
+++ unzipper/__init__.py
@@ -10,21 +10,21 @@ from config import Config
 boottime = time.time()
 
 plugins = dict(root="unzipper/modules")
-unzipperbot = Client(
-    "UnzipperBot",
-    bot_token=Config.BOT_TOKEN,
-    api_id=Config.APP_ID,
-    api_hash=Config.API_HASH,
-    plugins=plugins,
-    sleep_threshold=10,
-    max_concurrent_transmissions=3,
-)
 premiumuser = Client(
     "PremiumUser",
     session_string=Config.SESSION_STRING,
     api_id=Config.APP_ID,
     api_hash=Config.API_HASH,
     in_memory=False,
+    sleep_threshold=10,
+    max_concurrent_transmissions=3,
+)
+unzipperbot = Client(
+    "UnzipperBot",
+    bot_token=Config.BOT_TOKEN,
+    api_id=Config.APP_ID,
+    api_hash=Config.API_HASH,
+    plugins=plugins,
     sleep_threshold=10,
     max_concurrent_transmissions=3,
 )
