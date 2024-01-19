6fe9ff169867cccbd3182764bb1e3542f1f216eb
EDM115 <dev@edm115.eu.org>
2024-01-19 15:39:36 +0100

    shell

--- unzipper/modules/ext_script/ext_helper.py
+++ unzipper/modules/ext_script/ext_helper.py
@@ -31,20 +31,18 @@ async def cleanup_macos_artifacts(extraction_path):
 
 def __run_cmds_unzipper(command):
     cmd = shlex.split(command['cmd'])
-    ext_process = subprocess.Popen(
+    ext_cmd = subprocess.Popen(
         cmd,
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
-        shell=False
+        shell=True
     )
-    ext_out, ext_err = ext_process.communicate()
-
-    ext_out = ext_out.decode("utf-8").rstrip('\n')
-    ext_err = ext_err.decode("utf-8").rstrip('\n')
-
-    LOGGER.info(f"Output: {ext_out}")
-    if ext_err:
-        LOGGER.error(f"Error: {ext_err}")
+    ext_out = ext_cmd.stdout.read()[:-1].decode("utf-8").rstrip('\n')
+    LOGGER.info(ext_out)
+    if ext_cmd.stderr:
+        ext_cmd.stderr.close()
+    if ext_cmd.stdout:
+        ext_cmd.stdout.close()
     return ext_out
 
 
