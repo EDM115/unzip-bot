75c8e393f4864ab4a77ce7639a17a22094ab652f
EDM115 <git@edm115.eu.org>
2024-11-23 13:06:06 +0100

    test: env var issue in heroku (2)

--- start.sh
+++ start.sh
@@ -9,11 +9,15 @@ Copyright (c) 2022 - 2024 EDM115
 --> Follow EDM115 on Github
 "
 
-if [ -f .env ]; then
+if [ -f .env ] && [[ ! "$DYNO" =~ ^worker.* ]]; then
   if grep -qE '^[^#]*=\s*("|'\''?)\s*\1\s*$' .env; then
     echo "Some required vars are empty, please fill them unless you're filling them somewhere else (ex : Heroku, Docker Desktop)"
   else
-    export "$(grep -v '^#' .env | xargs)"
+    while IFS='=' read -r key value; do
+      if [[ ! $key =~ ^# && -n $key ]]; then
+        export "$key"="${value%\"}"
+      fi
+    done < .env
   fi
 fi
 
