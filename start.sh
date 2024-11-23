6f619a2456efc99f003dd28f0430a9ea18f680a7
EDM115 <git@edm115.eu.org>
2024-11-23 12:48:18 +0100

    test: env var issue in heroku

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 echo "
 🔥 unzip-bot 🔥
 
@@ -7,10 +8,18 @@ Copyright (c) 2022 - 2024 EDM115
 --> Join @EDM115bots on Telegram
 --> Follow EDM115 on Github
 "
-if grep -qE '^[^#]*=\s*("|'\''?)\s*\1\s*$' .env; then
-  echo "Some required vars are empty, please fill them unless you're filling them somewhere else (ex : Heroku, Docker Desktop)"
-else
-  source .env
+
+if [ -f .env ]; then
+  if grep -qE '^[^#]*=\s*("|'\''?)\s*\1\s*$' .env; then
+    echo "Some required vars are empty, please fill them unless you're filling them somewhere else (ex : Heroku, Docker Desktop)"
+  else
+    export "$(grep -v '^#' .env | xargs)"
+  fi
 fi
+
 export TZ="Europe/Paris"
+
+echo "Debug environment variables : "
+printenv
+
 exec python -m unzipbot
