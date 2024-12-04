EDM115/unzip-bot

commit 7438726a2d0655d34c6f711c392f4eeee7b41792

feat: v7.1.2a (lockfile on start) - creates a lock file on start - deleted it in case of errors/shutting down - restricts users from processing archives when the bot haven't started yet

unzipbot/__main__.py
@@ -80,6 +80,14 @@ if __name__ == "__main__":
     try:
         os.makedirs(Config.DOWNLOAD_LOCATION, exist_ok=True)
         os.makedirs(Config.THUMB_LOCATION, exist_ok=True)
+        lockfile = Config.DOWNLOAD_LOCATION + "/start.lock"
+
+        if os.path.exists(lockfile):
+            os.remove(lockfile)
+
+        with open(lockfile, "w") as lock_f:
+            pass  # create the lock file
+
         LOGGER.info(messages.get("main", "STARTING_BOT"))
         unzipbot_client.start()
         starttime = time.strftime("%Y/%m/%d - %H:%M:%S")
@@ -95,6 +103,7 @@ if __name__ == "__main__":
             removal(True)
             dl_thumbs()
             start_cron_jobs()
+            os.remove(lockfile)
             LOGGER.info(messages.get("main", "BOT_RUNNING"))
             idle()
         else:
@@ -106,8 +115,10 @@ if __name__ == "__main__":
             except:
                 pass
 
+            os.remove(lockfile)
             shutdown_bot()
     except Exception as e:
         LOGGER.error(messages.get("main", "ERROR_MAIN_LOOP", None, e))
     finally:
+        os.remove(lockfile)
         shutdown_bot()

Dockerfile
@@ -26,7 +26,7 @@ RUN pip install -U pip setuptools wheel && \
 
 FROM python:3.12-alpine
 
-ARG VERSION="7.1.1a"
+ARG VERSION="7.1.2a"
 
 LABEL org.opencontainers.image.authors="EDM115 <unzip@edm115.dev>"
 LABEL org.opencontainers.image.base.name="python:3.12-alpine"

config.py
@@ -32,4 +32,4 @@ class Config:
     MONGODB_DBNAME = os.environ.get("MONGODB_DBNAME", "Unzipper_Bot")
     TG_MAX_SIZE = 2097152000
     THUMB_LOCATION = f"{os.path.dirname(__file__)}/Thumbnails"
-    VERSION = os.environ.get("UNZIPBOT_VERSION", "7.1.1a")
+    VERSION = os.environ.get("UNZIPBOT_VERSION", "7.1.2a")

unzipbot/modules/commands.py
@@ -179,6 +179,11 @@ async def extract_archive(_, message: Message):
             return
 
         user_id = message.from_user.id
+
+        if os.path.exists(Config.DOWNLOAD_LOCATION + "/start.lock"):
+            await message.reply(messages.get("commands", "STILL_STARTING", user_id))
+            return
+
         unzip_msg = await message.reply(
             messages.get("commands", "PROCESSING2", user_id),
             reply_to_message_id=message.id,

commit 47644cc7a2cfa19aa1d3bfdeb1e6c270f123b6ee

fix(crash): fixes issues introduced in 7438726a2d0655d34c6f711c392f4eeee7b41792 and 898819dd079f30b47ed86b40870f1f6f3e9488bf

unzipbot/__main__.py
@@ -79,7 +79,6 @@ def shutdown_bot():
 
 if __name__ == "__main__":
     try:
-        shutil.rmtree(Config.DOWNLOAD_LOCATION)
         os.makedirs(Config.DOWNLOAD_LOCATION, exist_ok=True)
         os.makedirs(Config.THUMB_LOCATION, exist_ok=True)
 
@@ -104,6 +103,9 @@ if __name__ == "__main__":
             removal(True)
             dl_thumbs()
             start_cron_jobs()
+            # clean previous downloads on volumes
+            shutil.rmtree(Config.DOWNLOAD_LOCATION)
+            os.makedirs(Config.DOWNLOAD_LOCATION, exist_ok=True)
             os.remove(Config.LOCKFILE)
             LOGGER.info(messages.get("main", "BOT_RUNNING"))
             idle()
@@ -121,5 +123,6 @@ if __name__ == "__main__":
     except Exception as e:
         LOGGER.error(messages.get("main", "ERROR_MAIN_LOOP", None, e))
     finally:
-        os.remove(Config.LOCKFILE)
+        if os.path.exists(Config.LOCKFILE):
+            os.remove(Config.LOCKFILE)
         shutdown_bot()